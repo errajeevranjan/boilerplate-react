2d272a03e81ac58cee085a0d4136d301aacef6a2
errajeevranjan <rjeevrnjn17@gmail.com>
2021-11-03 00:09:33 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+	return (
+		<div className='App'>
+			Welcome to boilerplate code for react with TS + react-query +
+			redux-toolkit + mui.
+		</div>
+	);
+};
+
+export default App;
