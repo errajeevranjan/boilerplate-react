85f21b6a9b585b5c374c9311cdad6053e6e6bf25
errajeevranjan <rjeevrnjn17@gmail.com>
2021-11-03 01:48:12 +0530

    setup redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { messageText, testReducer } from "./redux/app/appSlice";
+import { useAppSelector, useAppDispatch } from "./redux/ReduxHooks";
+
 const App = () => {
+	const message = useAppSelector(messageText);
+	const dispatch = useAppDispatch();
 	return (
 		<div className='App'>
 			Welcome to boilerplate code for react with TS + react-query +
 			redux-toolkit + mui.
+			<h1>{message}</h1>
+			<button onClick={() => dispatch(testReducer())}>
+				Test if reducer is working or not
+			</button>
 		</div>
 	);
 };
---
+++ src/redux/app/appSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../ReduxStore';
+
+
+export interface CounterState {
+  message: string;
+  status: 'idle' | 'loading' | 'failed';
+}
+
+const initialState: CounterState = {
+  message: 'If you can see this message then you have working redux setup!',
+  status: 'idle',
+};
+
+
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    testReducer: (state) => {
+      state.message = 'If you can see this message then your reducer setup is correct!';
+    },
+
+  },
+
+});
+
+// exporting reducer so that we can dispatch this in desired page(s) which then will trigger associated action with the reducer
+export const { testReducer } = appSlice.actions;
+
+// exporting value of message for consumption by page(s)
+export const messageText = (state: RootState) => state.app.message;
+
+// exporting the entire slice
+export default appSlice.reducer;
