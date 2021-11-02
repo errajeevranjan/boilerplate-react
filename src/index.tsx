c66bf7fa1463e3e63fcc918c70da363a8dcb2057
errajeevranjan <rjeevrnjn17@gmail.com>
2021-11-03 03:17:38 +0530

    setup react-query and react-query-dev-tool

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import App from "App";
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { Provider } from "react-redux";
 import { store } from "reduxStore";
 import "styles/globalStyles.scss";
+
+const queryClient = new QueryClient();
+
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<App />
+			</Provider>
+			<ReactQueryDevtools
+			// initialIsOpen //! uncomment this to keep react-query-dev-tools open by default
+			/>
+		</QueryClientProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
