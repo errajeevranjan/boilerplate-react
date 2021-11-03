4a16d0b00f70d97f08dce2f71db0c2af08c96232
errajeevranjan <rjeevrnjn17@gmail.com>
2021-11-03 23:37:25 +0530

    created overrides file to modify default styling of  mui components

--- src/theme/Theme.ts
+++ src/theme/Theme.ts
@@ -1,13 +1,17 @@
 import { createTheme } from '@mui/material/styles';
+import { overrides } from './Overrides';
+const {components}=overrides
 
 export const darkTheme = createTheme({
   palette: {
     mode:'dark',
   },
+  components
 });
 
 export const lightTheme = createTheme({
   palette: {
     mode:'light',
   },
+  components
 });
