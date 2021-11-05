e82e98580d56de0e9cc997bc9a73948d3de0879c
errajeevranjan <rjeevrnjn17@gmail.com>
2021-11-05 21:26:34 +0530

    Refactored app slice and redux code in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import {
-	messageText,
-	testReducer,
-	darkModeStatus,
-	themeReducer,
-} from "reduxStore/app/appSlice";
-import { useAppDispatch, useAppSelector } from "reduxStore";
-import { ThemeProvider } from "@mui/material/styles";
-import { lightTheme, darkTheme } from "theme/Theme";
 import { Button, Paper } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import Navigation from "components/navigation/Navigation";
+import { useAppDispatch, useAppSelector } from "reduxStore";
+import {
+	appDataInReduxStore,
+	testReducer,
+	toggleDarkModeReducer,
+} from "reduxStore/app/appSlice";
+import { darkTheme, lightTheme } from "theme/Theme";
 
 const App = () => {
-	const message = useAppSelector(messageText);
-	const isDarkModeActive = useAppSelector(darkModeStatus);
+	// const appDataInReduxStore  = useAppSelector((state) => state.app); //! alternate way to access entire object of certain slice
+	const { message, isDarkModeActive } = useAppSelector(appDataInReduxStore);
+
 	const dispatch = useAppDispatch();
 
 	return (
 		<ThemeProvider theme={isDarkModeActive ? darkTheme : lightTheme}>
 			<Paper sx={{ height: "100vh" }}>
-				Welcome to boilerplate code for react with TS + react-query +
-				redux-toolkit + mui.
-				<h1>{message}</h1>
-				<button onClick={() => dispatch(testReducer())}>
-					Test if reducer is working or not
-				</button>
-				<Button
-					variant='contained'
-					color='secondary'
-					onClick={() => dispatch(themeReducer())}>
-					Toggle Theme
-				</Button>
+				<Navigation>
+					Welcome to boilerplate code for react with TS + react-query +
+					redux-toolkit + mui.
+					<h1>{message}</h1>
+					<button onClick={() => dispatch(testReducer())}>
+						Test if reducer is working or not
+					</button>
+					<Button
+						variant='contained'
+						color='secondary'
+						onClick={() => dispatch(toggleDarkModeReducer())}>
+						Toggle Theme
+					</Button>
+				</Navigation>
 			</Paper>
 		</ThemeProvider>
 	);
--- src/reduxStore/app/appSlice.ts
+++ src/reduxStore/app/appSlice.ts
@@ -1,45 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { RootState } from '..';
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "..";
 
-
-export interface AppState {
-  message: string;
-  status: 'idle' | 'loading' | 'failed';
-  isDarkModeActive:boolean;
-}
-
-const initialState: AppState = {
-  message: 'If you can see this message then you have working redux setup!',
-  status: 'idle',
-  isDarkModeActive:false,
-
+type InitialState = {
+	message: string;
+	status: "idle" | "loading" | "failed";
+	isDarkModeActive: boolean;
 };
 
-
+const initialState: InitialState = {
+	message: "If you can see this message then you have working redux setup!",
+	status: "idle",
+	isDarkModeActive: false,
+};
 
 export const appSlice = createSlice({
-  name: 'app',
-  initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
-  reducers: {
-    testReducer: (state) => {
-      state.message = 'If you can see this message then your reducer setup is correct!';
-    },
+	name: "app",
+	initialState,
+	// The `reducers` field lets us define reducers and generate associated actions
+	reducers: {
+		testReducer: (state) => {
+			state.message =
+				"If you can see this message then your reducer setup is correct!";
+		},
 
-    themeReducer:(state)=>{
-      state.isDarkModeActive=!state.isDarkModeActive
-    }
-
-  },
-
+		toggleDarkModeReducer: (state) => {
+			state.isDarkModeActive = !state.isDarkModeActive;
+		},
+	},
 });
 
 // exporting reducer so that we can dispatch this in desired page(s) which then will trigger associated action with the reducer
-export const { testReducer,themeReducer } = appSlice.actions;
+export const { testReducer, toggleDarkModeReducer } = appSlice.actions;
 
-// exporting value of message for consumption by page(s)
-export const messageText = (state: RootState) => state.app.message;
-export const darkModeStatus = (state:RootState)=> state.app.isDarkModeActive
+// exporting the app data object for consumption by page(s)
+export const appDataInReduxStore = (state: RootState) => state.app;
 
 // exporting the entire slice
 export default appSlice.reducer;
