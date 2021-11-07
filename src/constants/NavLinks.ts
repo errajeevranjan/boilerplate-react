f81aab26172d90eb95e47d7e9d4e87027adc7146
errajeevranjan <rjeevrnjn17@gmail.com>
2021-11-07 21:27:51 +0530

    Updated path to match that of route.ts

--- src/constants/NavLinks.ts
+++ src/constants/NavLinks.ts
@@ -4,37 +4,37 @@ export const NAV_LINKS = [
 	{
 		key: "abc1",
 		title: "Link title abc1",
-		path: "/link-path-abc1",
+		path: "/route-1",
 		icon: MdHomeFilled,
 	},
 	{
 		key: "abc2",
 		title: "Link title abc2",
-		path: "/link-path-abc2",
+		path: "/route-2",
 		icon: MdHomeFilled,
 	},
 	{
 		key: "abc3",
 		title: "Link title abc3",
-		path: "/link-path-abc3",
+		path: "/route-3",
 		icon: MdHomeFilled,
 	},
 	{
 		key: "abc4",
 		title: "Link title abc4",
-		path: "/link-path-abc4",
+		path: "/route-4",
 		icon: MdHomeFilled,
 	},
 	{
 		key: "abc5",
 		title: "Link title abc5",
-		path: "/link-path-abc5",
+		path: "/route-5",
 		icon: MdHomeFilled,
 	},
 	{
 		key: "abc6",
 		title: "Link title abc6",
-		path: "/link-path-abc6",
+		path: "/route-6",
 		icon: MdHomeFilled,
 	},
 ];
