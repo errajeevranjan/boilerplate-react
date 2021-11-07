dbdf1f42189f2ccb00e26d862b87d3f05758b074
errajeevranjan <rjeevrnjn17@gmail.com>
2021-11-07 23:08:03 +0530

    Implemented axios and react-query in homepage and  updated homepageTwo

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,28 +1,55 @@
-import { Button } from "@mui/material";
+import { Button, AlertColor } from "@mui/material";
 import { TestImage } from "assets";
 import { useAppDispatch, useAppSelector } from "reduxStore";
 import { appDataInReduxStore, testReducer } from "reduxStore/app/appSlice";
 import { useState } from "react";
 import PositionedSnackbar from "common/mui/PositionedSnackbar";
 
+import { useApiTest } from "hooks/useApiTest";
+
+type PromiseType = {
+	a: {};
+	b: {};
+};
+
 const HomePage = () => {
 	const dispatch = useAppDispatch();
-	const { message } = useAppSelector(appDataInReduxStore);
+	const { welcomeText } = useAppSelector(appDataInReduxStore);
 	const [open, setOpen] = useState(false);
+	const [severity, setSeverity] = useState<AlertColor | "success">("success");
+	const [message, setMessage] = useState("");
+
+	const onSuccess = (a: PromiseType) => {
+		setOpen(true);
+		setSeverity("success");
+		setMessage("Axios request successful check react-query-dev-tool!");
+	};
+
+	const onError = (b: PromiseType) => {
+		setOpen(true);
+		setSeverity("error");
+		setMessage("Axios request failed react-query-dev-tool!");
+	};
+
+	const { isLoading, data, isError, error } = useApiTest({
+		onSuccess,
+		onError,
+	});
+
+	console.log({ isLoading, data, isError, error });
 
 	return (
 		<>
 			This is home page <b>ONE</b> of the app Welcome to boilerplate code for
 			react with TS + react-query + redux-toolkit + mui.
-			<h1>{message}</h1>
+			<h1>{welcomeText}</h1>
 			<Button onClick={() => dispatch(testReducer())}>
 				Test if reducer is working or not
 			</Button>
 			<img src={TestImage} alt='testImage' />
-			<Button onClick={() => setOpen(!open)}> Check snackbar</Button>
 			<PositionedSnackbar
-				message='Success: Snackbar is working!'
-				messageType='success'
+				message={message}
+				severity={severity}
 				open={open}
 				setOpen={setOpen}
 			/>
--- src/pages/homePage/HomePageTwo.tsx
+++ src/pages/homePage/HomePageTwo.tsx
@@ -7,14 +7,14 @@ import PositionedSnackbar from "common/mui/PositionedSnackbar";
 
 const HomePageTwo = () => {
 	const dispatch = useAppDispatch();
-	const { message } = useAppSelector(appDataInReduxStore);
+	const { welcomeText } = useAppSelector(appDataInReduxStore);
 	const [open, setOpen] = useState(false);
 
 	return (
 		<>
 			This is home page <b> TWO </b> of the app Welcome to boilerplate code for
 			react with TS + react-query + redux-toolkit + mui.
-			<h1>{message}</h1>
+			<h1>{welcomeText}</h1>
 			<Button onClick={() => dispatch(testReducer())}>
 				Test if reducer is working or not
 			</Button>
@@ -22,7 +22,7 @@ const HomePageTwo = () => {
 			<Button onClick={() => setOpen(!open)}> Check snackbar</Button>
 			<PositionedSnackbar
 				message='Success: Snackbar is working!'
-				messageType='success'
+				severity='success'
 				open={open}
 				setOpen={setOpen}
 			/>
